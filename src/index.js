eeacf56a8be2326addaf084b39b001fe15e82f9d
NetoCruz <galaxy.net0710@gmail.com>
2020-01-13 18:49:43 -0600

    Instalando Elint y gitignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 
-ReactDOM.render(<HelloWorld/>,document.getElementById('app'))
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
